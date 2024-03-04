c455544a2845d3dd188ac9538f1e2c8ee06bf316
Bruno Skëndaj <skendajbruno07@gmail.com>
2024-03-04 23:25:54 +0100

    Initial commit
    Generated by create-expo-stack 2.0.0.

---
+++ app/modal.tsx
@@ -0,0 +1,23 @@
+ 
+    import { YStack, Paragraph, Separator, Theme } from "tamagui";
+	import { Platform } from 'react-native'
+
+import { StatusBar } from "expo-status-bar";
+
+import EditScreenInfo from "../components/edit-screen-info";
+
+export default function ModalScreen() {
+    
+        return (
+			<Theme name="light">
+				<YStack flex={1} alignItems="center" justifyContent="center">
+					<StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
+					<Paragraph>Modal</Paragraph>
+					<Separator />
+					<EditScreenInfo path="app/modal.tsx" />
+				</YStack>
+			</Theme>
+		);
+    
+}
+
